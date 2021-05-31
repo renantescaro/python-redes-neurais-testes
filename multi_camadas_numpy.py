01eefbbbb78bf969a9f68d47fdeafd81458d0f17
renantescaro <renantescaro@yahoo.com.br>
2021-05-30 23:43:51 -0300

    calculo dos erros de saida

--- multi_camadas_numpy.py
+++ multi_camadas_numpy.py
@@ -3,7 +3,7 @@ import numpy as np
 class Rede:
     def __init__(self):
         self.entradas = [[0,0], [0,1], [1,0], [1,1]]
-        self.saidas   = [0, 1, 1, 0]
+        self.saidas   = [[0], [1], [1], [0]]
         self.pesos_camada_oculta = [
             [-0.424, -0.740, -0.961],
             [-0.358, -0.577, -0.469]]
@@ -23,12 +23,23 @@ class Rede:
         camada_oculta       = self.ativacao(soma_sinapse_oculta)
         soma_sinapse_saida  = np.dot(camada_oculta, self.pesos_camada_saida)
         camada_saida        = self.ativacao(soma_sinapse_saida)
-        print(camada_saida)
+        erro_camada_saida   = self.calcular_valor_erro(self.saidas, camada_saida)
+        media_absoluta      = self.media_absoluta_erro(erro_camada_saida)
+        print(media_absoluta)
+        print(erro_camada_saida)
 
 
     def ativacao(self, soma) -> float:
         # sigmoid function
         return 1 / ( 1 + np.exp(-soma) )
+
+
+    def calcular_valor_erro(self, valor_esperado, resposta_obtida) -> float:
+        return valor_esperado - resposta_obtida
+
+    
+    def media_absoluta_erro(self, erro_camada_saida):
+        return np.mean(np.abs(erro_camada_saida))
 
 
 Rede()
