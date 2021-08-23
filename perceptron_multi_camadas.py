995196b7e1ee390cf4f40455b2cbc5cc6f7a3ec3
renantescaro <renantescaro@yahoo.com.br>
2021-08-23 19:12:18 -0300

    movendo funções de ativação para classe separada

--- perceptron_multi_camadas.py
+++ perceptron_multi_camadas.py
@@ -1,8 +1,12 @@
 import numpy as np
+from ativacao import Ativacao
 
 
 class Rede:
     def __init__(self):
+        # classe com as ativações e suas derivadas
+        self.ativacao = Ativacao()
+
         self.entradas = np.array([
             [0,0],
             [0,1],
@@ -39,10 +43,10 @@ class Rede:
     def calcular(self):
         for epoca in range(self.epocas):
             soma_sinapse_oculta   = np.dot(self.entradas, self.pesos_camada_oculta)
-            camada_oculta_ativada = self.ativacao(soma_sinapse_oculta)
+            camada_oculta_ativada = self.ativacao.ativacao(soma_sinapse_oculta)
             print('Resultados: '+str(soma_sinapse_oculta))
             soma_sinapse_saida    = np.dot(camada_oculta_ativada, self.pesos_camada_saida)
-            camada_saida_ativada  = self.ativacao(soma_sinapse_saida)
+            camada_saida_ativada  = self.ativacao.ativacao(soma_sinapse_saida)
             self.resultados       = camada_saida_ativada
 
             # calculo do erro
@@ -51,8 +55,8 @@ class Rede:
             #print('Erro: '+str(media_absoluta))
 
             # calcula Deltas
-            delta_saida  = erro_camada_saida * self.ativacao_derivada(camada_saida_ativada)
-            delta_oculta = (delta_saida.dot(self.pesos_camada_saida.T)) * (self.ativacao_derivada(camada_oculta_ativada))
+            delta_saida  = erro_camada_saida * self.ativacao.ativacao_derivada(camada_saida_ativada)
+            delta_oculta = (delta_saida.dot(self.pesos_camada_saida.T)) * (self.ativacao.ativacao_derivada(camada_oculta_ativada))
 
             # calcula novos pesos da camada de saida
             pesos_saida_novos       = camada_oculta_ativada.T.dot(delta_saida)
@@ -61,46 +65,12 @@ class Rede:
             # calcula novos pesos da camada oculta
             pesos_oculta_novos       = self.entradas.T.dot(delta_oculta)
             self.pesos_camada_oculta = (self.pesos_camada_oculta * self.momento) + (pesos_oculta_novos * self.apredizagem)
-            
+
 
         print('\n ------------------------ \n ')
         print('Saidas:\n' + str(self.resultados))
         # print('Pesos Camada Saida \n' + str(self.pesos_camada_saida))
         # print('Pesos Camada Oculta \n' + str(self.pesos_camada_oculta))
-
-
-    def ativacao(self, soma):
-        return self.leaky_re_lu(soma)
-
-
-    def ativacao_derivada(self, valor_ativacao):
-        return self.leaky_re_lu_derivada(valor_ativacao)
-
-
-    def re_lu(self, x):
-        return x * (x > 0)
-
-
-    def re_lu_derivada(self, x):
-        return 1. * (x > 0)
-
-    
-    def leaky_re_lu(self, x):
-        return np.where(x > 0, x, x * 0.01)
-    
-
-    def leaky_re_lu_derivada(self, x):
-        dx = np.ones_like(x)
-        dx[x < 0] = 0.01
-        return dx
-
-
-    def sigmoid(self, soma):
-        return 1 / ( 1 + np.exp(-soma) )
-
-
-    def sigmoid_derivada(self, valor_ativacao):
-        return valor_ativacao * (1 - valor_ativacao)
 
 
 Rede()
