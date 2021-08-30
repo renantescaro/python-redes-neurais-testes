3a440633df5939c4c43b09b4fee2b0f77e6fe4dc
renantescaro <renantescaro@yahoo.com.br>
2021-08-30 17:09:36 -0300

    inicialização e carregando imagem3

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
 from perceptron_multi_camadas import PerceptronMultiCamadas
+from imagem import Imagem
 
-PerceptronMultiCamadas().calcular()
+foto_numero = Imagem('1.png')
+# print(foto_numero.data)
+
+# qtd = 1
+# for linha in foto_numero.data:
+#     print(str(qtd)+': '+str(linha))
+#     qtd +=1
+
+
+PerceptronMultiCamadas(
+    entradas = foto_numero.data,
+    saidas   = []
+).calcular()
