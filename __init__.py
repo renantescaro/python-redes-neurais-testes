e69b03d32ecba5d0a73f05ccf5dc54420831667f
renantescaro <renantescaro@yahoo.com.br>
2021-08-30 21:08:44 -0300

    recebendo array numpy das imagens e iniciando treinamento de rede

--- __init__.py
+++ __init__.py
@@ -1,16 +1,20 @@
-from perceptron_multi_camadas import PerceptronMultiCamadas
-from imagem import Imagem
-
-foto_numero = Imagem('1.png')
-# print(foto_numero.data)
-
-# qtd = 1
-# for linha in foto_numero.data:
-#     print(str(qtd)+': '+str(linha))
-#     qtd +=1
+import numpy as np
+from classes.imagem import Imagem
+from classes.treinamento_perceptron_multi_camadas import TreinamentoPerceptronMultiCamadas
 
 
-PerceptronMultiCamadas(
-    entradas = foto_numero.data,
-    saidas   = []
+entradas = np.array([
+    Imagem('assets/0_pequena1.png').array(),
+    Imagem('assets/0_pequena2.png').array(),
+    Imagem('assets/1_pequena1.png').array(),
+    Imagem('assets/1_pequena2.png').array() ])
+
+saidas = np.array([[0], [0], [1], [1]])
+
+# entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
+# saidas   = np.array([[0], [1], [1], [0]])
+
+TreinamentoPerceptronMultiCamadas(
+    entradas = entradas,
+    saidas   = saidas
 ).calcular()
