b4df9b8078d744c81e28123a6fcba3e9f79d0131
renantescaro <renantescaro@yahoo.com.br>
2021-08-30 21:11:36 -0300

    adicionando return nas funções de ativação

--- tests/ativacao_test.py
+++ tests/ativacao_test.py
@@ -1,29 +1,31 @@
 import unittest
 import sys
 sys.path.append('C:\_TCC_\python-redes-neurais-testes')
-from ativacao import Ativacao
+from classes.ativacao import Ativacao
 
 
 class AtivacaoTest:
     def re_lu_test(self, x):
-        Ativacao().re_lu(x)
-
+        return Ativacao().re_lu(x)
 
     def re_lu_derivada_test(self, x):
-        Ativacao().re_lu_derivada(x)
+        return Ativacao().re_lu_derivada(x)
 
     
     def leaky_re_lu_test(self, x):
-        Ativacao().leaky_re_lu(x)
+        return Ativacao().leaky_re_lu(x)
     
 
     def leaky_re_lu_derivada_test(self, x):
-        Ativacao().leaky_re_lu_derivada(x)
+        return Ativacao().leaky_re_lu_derivada(x)
 
 
     def sigmoid_test(self, x):
-        Ativacao().sigmoid(x)
+        return Ativacao().sigmoid(x)
 
 
     def sigmoid_derivada_test(self, x):
-        Ativacao().sigmoid_derivada(x)
+        return Ativacao().sigmoid_derivada(x)
+
+aaa = AtivacaoTest().sigmoid_test(-1)
+print(aaa)
