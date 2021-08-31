7b7b2e4d540a550136596e79c96f2c69c7f59052
renantescaro <renantescaro@yahoo.com.br>
2021-08-30 21:07:54 -0300

    movendo para pasta classes

--- imagem.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-class Imagem:
-    def __init__(self, caminho_arquivo):
-        imagem     = Image.open(caminho_arquivo)
-        self.data  = np.array(imagem.convert('L'))
-        self.type  = type(self.data)
-        self.shape = self.data.shape
