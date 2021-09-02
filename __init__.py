42ca9736f19c34c8e2b70b899a2dde981a0945b3
renantescaro <renantescaro@yahoo.com.br>
2021-09-02 15:45:36 -0300

    adicionando parametros de entrada e saida de forma dinamica pela leitura das imagens

--- __init__.py
+++ __init__.py
@@ -1,87 +1,17 @@
 import numpy as np
-from classes.imagem import Imagem
+import sys
+sys.path.append('C:\_TCC_\python-redes-neurais-testes')
+from classes.grafico import Grafico
+from classes.parametro import Parametro
 from classes.treinamento_perceptron_multi_camadas import TreinamentoPerceptronMultiCamadas
 
-# Entrada().ler_imagens()
 
-entradas = np.array([
-    # 0
-    Imagem('assets/treinamento/0_1.png').array(),
-    Imagem('assets/treinamento/0_2.png').array(),
+parametro = Parametro()
+parametro.carregar_assets_treinamento()
 
-    # 1
-    Imagem('assets/treinamento/1_1.png').array(),
-    Imagem('assets/treinamento/1_2.png').array(),
+treinamento = TreinamentoPerceptronMultiCamadas(
+    entradas = np.array(parametro.entradas),
+    saidas   = np.array(parametro.saidas) )
+treinamento.calcular()
 
-    # 2
-    Imagem('assets/treinamento/2_1.png').array(),
-    Imagem('assets/treinamento/2_2.png').array(),
-
-    # 3
-    Imagem('assets/treinamento/3_1.png').array(),
-    Imagem('assets/treinamento/3_2.png').array(),
-
-    # 4
-    Imagem('assets/treinamento/4_1.png').array(),
-    Imagem('assets/treinamento/4_2.png').array(),
-
-    # 5
-    Imagem('assets/treinamento/5_1.png').array(),
-    Imagem('assets/treinamento/5_2.png').array(),
-
-    # 6
-    Imagem('assets/treinamento/6_1.png').array(),
-    Imagem('assets/treinamento/6_2.png').array(),
-
-    # 7
-    Imagem('assets/treinamento/7_1.png').array(),
-    Imagem('assets/treinamento/7_2.png').array(),
-
-    # 8
-    Imagem('assets/treinamento/8_1.png').array(),
-    Imagem('assets/treinamento/8_2.png').array(),
-
-    # 9
-    Imagem('assets/treinamento/9_1.png').array(),
-    Imagem('assets/treinamento/9_2.png').array(),
-], dtype=np.longdouble)
-
-saidas = np.array([
-    # 0
-    [0,0,0,0,0,0], [0,0,0,0,0,0],
-
-    # 1
-    [0,0,0,0,0,1], [0,0,0,0,0,1],
-
-    # 2
-    [0,0,0,0,1,0], [0,0,0,0,1,0],
-
-    # 3
-    [0,0,0,0,1,1], [0,0,0,0,1,1],
-
-    # 4
-    [0,0,0,1,0,0], [0,0,0,1,0,0],
-
-    # 5
-    [0,0,0,1,0,1], [0,0,0,1,0,1],
-
-    # 6
-    [0,0,0,1,1,0], [0,0,0,1,1,0],
-
-    # 7
-    [0,0,0,1,1,1], [0,0,0,1,1,1],
-
-    # 8
-    [0,0,1,0,0,0], [0,0,1,0,0,0],
-
-    # 9
-    [0,0,1,0,0,1], [0,0,1,0,0,1]
-], dtype=np.longdouble)
-
-# entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
-# saidas   = np.array([[0], [1], [1], [0]])
-
-TreinamentoPerceptronMultiCamadas(
-    entradas = entradas,
-    saidas   = saidas
-).calcular()
+Grafico().medias_absolutas(treinamento.medias_absolutas)
