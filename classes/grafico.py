3fe86859e57df0439e2a6fd9f3efbf9edcee2694
renantescaro <renantescaro@yahoo.com.br>
2021-09-02 15:48:39 -0300

    classe para a geração de graficos

---
+++ classes/grafico.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+class Grafico:
+    def medias_absolutas(self, dados):
+        fig, ax = plt.subplots()
+        ax.plot(dados)
+        ax.set(
+            ylabel = '% Erro',
+            xlabel = 'Épocas',
+            title  = '% Erro por Épocas' )
+        ax.grid()
+        plt.show()
