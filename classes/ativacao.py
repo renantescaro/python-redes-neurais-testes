f8cbff515596cfcd5825ce55d001bfca2e5aa8aa
renantescaro <renantescaro@yahoo.com.br>
2022-04-11 22:15:42 -0300

    alterando métodos publicos para privado

--- classes/ativacao.py
+++ classes/ativacao.py
@@ -1,36 +1,35 @@
 import numpy as np
-# import tensorflow as tf
 
 
 class Ativacao:
     def ativacao(self, soma):
-        return self.sigmoid(soma)
+        return self._sigmoid(soma)
 
 
     def ativacao_derivada(self, valor_ativacao):
-        return self.sigmoid_derivada(valor_ativacao)
+        return self._sigmoid_derivada(valor_ativacao)
 
 
-    def re_lu(self, x):
+    def _re_lu(self, x):
         return x * (x > 0)
 
 
-    def re_lu_derivada(self, x):
+    def _re_lu_derivada(self, x):
         return 1. * (x > 0)
 
 
-    def leaky_re_lu(self, x):
+    def _leaky_re_lu(self, x):
         return np.where(x > 0, x, x * 0.01).astype(np.float)
 
 
-    def leaky_re_lu_derivada(self, x):
+    def _leaky_re_lu_derivada(self, x):
         return np.sum([
             ((x > 0) * x),
             ((x <= 0) * x * 0.01)],
             dtype=np.float )
 
 
-    def elu(self, x, alpha=1):
+    def _elu(self, x, alpha=1):
         less_than_zero =  (np.zeros_like(x) >= x).astype(np.float)
         less_than_zero *= (np.exp(x) - 1)*alpha
         grt_than_zero  =  (np.zeros_like(x) < x).astype(np.float)
@@ -38,24 +37,24 @@ class Ativacao:
         return less_than_zero + grt_than_zero
 
 
-    def elu_derivada(self, x, alpha=1):
+    def _elu_derivada(self, x, alpha=1):
         less_than_zero =  (np.zeros_like(x) >= x).astype(np.float)
-        less_than_zero *= self.elu(x)+alpha
+        less_than_zero *= self._elu(x)+alpha
         grt_than_zero  =  (np.zeros_like(x) < x).astype(np.float)
         return less_than_zero + grt_than_zero
 
 
-    def sigmoid(self, soma):
+    def _sigmoid(self, soma):
         return 1 / ( 1 + np.exp(-soma) )
 
 
-    def sigmoid_derivada(self, valor_ativacao):
+    def _sigmoid_derivada(self, valor_ativacao):
         return valor_ativacao * (1 - valor_ativacao)
 
 
-    def tanh(self, x):
-        return 2*self.sigmoid(2*x) - 1
+    def _tanh(self, x):
+        return 2*self._sigmoid(2*x) - 1
 
 
-    def tanh_derivada(self, x):
-        return 1 - self.tanh(x) ** 2
+    def _tanh_derivada(self, x):
+        return 1 - self._tanh(x) ** 2
