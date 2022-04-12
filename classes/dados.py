a0e50e607c660459ae58b228914a3d7727d27f05
renantescaro <renantescaro@yahoo.com.br>
2022-04-11 22:16:08 -0300

    classe para salvar e resgatar valores dos pesos

---
+++ classes/dados.py
@@ -0,0 +1,26 @@
+from typing import Any
+from numpy import savetxt
+from numpy import loadtxt
+
+
+class Dados:
+    def __init__(self, versao=None) -> None:
+        self._caminho = 'assets/pesos/'
+        self._tipo = '.csv'
+        self.versao = versao
+
+
+    def _caminho_arquivo(self, chave:str) -> str:
+        if self.versao is None:
+            return f'{self._caminho}{chave}{self._tipo}'
+        return f'{self._caminho}{chave}_{self.versao}_{self._tipo}'
+
+
+    def set(self, chave:str, valor:Any) -> None:
+        arquivo = self._caminho_arquivo(chave)
+        savetxt(arquivo, valor, delimiter=',')
+
+
+    def get(self, chave:str):
+        arquivo = self._caminho_arquivo(chave)
+        return loadtxt(arquivo, delimiter=',')
