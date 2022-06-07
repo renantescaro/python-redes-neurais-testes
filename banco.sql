77ea61fb6886319cb0f49f58c78ed798aed6d2a4
renantescaro <renantescaro@yahoo.com.br>
2022-06-06 22:17:34 -0300

    atualizando tabela de 'parametro' para 'registro'

--- banco.sql
+++ banco.sql
@@ -1,12 +1,12 @@
 
 CREATE DATABASE rede_neural;
 
-CREATE TABLE parametro (
+CREATE TABLE registro(
   id int NOT NULL AUTO_INCREMENT,
   qtd_neuronios_camada_oculta int DEFAULT NULL,
   apredizagem float DEFAULT NULL,
   porcentagem_erro float DEFAULT NULL,
-  data_inserido timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-
+  data_inserido datetime DEFAULT NULL,
+  data_finalizado datetime DEFAULT NULL,
   PRIMARY KEY (id)
-)
+);
